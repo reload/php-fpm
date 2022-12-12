dc9aa676038785bc3c40e5d3d88e308bd7d8d884
Arne Jørgensen <arne@arnested.dk>
2022-12-12 10:12:11 +0100

    Use different shellcheck action
    The new action also runs shfmt.
    
    Fix existing shell script with shfmt.

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,8 +3,8 @@
 mapfile -t versions < <(grep -E '^FROM .* AS php[0-9\.]+$' Dockerfile | cut -f 4 -d ' ')
 
 for version in "${versions[@]}"; do
-    image=$(grep "AS ${version}" Dockerfile | cut -f 2 -d ' ' | cut -f 1 -d @)
-    new=$(docker manifest inspect -v "${image}" | jq -r '.[0].Ref')
+	image=$(grep "AS ${version}" Dockerfile | cut -f 2 -d ' ' | cut -f 1 -d @)
+	new=$(docker manifest inspect -v "${image}" | jq -r '.[0].Ref')
 
-    sed -i "s#FROM .*AS ${version}#FROM ${new} AS ${version}#" Dockerfile
+	sed -i "s#FROM .*AS ${version}#FROM ${new} AS ${version}#" Dockerfile
 done
