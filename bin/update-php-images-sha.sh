6fce348daee70956e3daa49a890d5331e7fe3d87
Arne Jørgensen <arne@arnested.dk>
2024-09-06 10:36:20 +0200

    Let update script find multiarch SHA's
    Update PHP images to use multiarch SHA's

--- bin/update-php-images-sha.sh
+++ bin/update-php-images-sha.sh
@@ -6,6 +6,7 @@ mapfile -t versions < <(grep -E '^FROM .* AS php[0-9\.]+$' Dockerfile | cut -f 4
 
 for version in "${versions[@]}"; do
 	image=$(grep "AS ${version}" Dockerfile | cut -f 2 -d ' ' | cut -f 1 -d @)
-	new=$(docker buildx imagetools inspect --raw "${image}" | jq -r '.manifests[0].digest')
+	docker pull "${image}"
+	new=$(docker image inspect "${image}" | jq -r '.[0].RepoDigests[0]' | cut -f 2 -d @)
 	sed -i "s#FROM .*AS ${version}#FROM ${image}@${new} AS ${version}#" Dockerfile
 done
